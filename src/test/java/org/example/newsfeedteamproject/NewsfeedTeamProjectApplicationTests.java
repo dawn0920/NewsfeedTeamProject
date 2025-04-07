cf35ada7270ecadb7d0c479c0f6aa751fff6f2e0
dawn0920 <saoul0920@gmail.com>
2025-04-07 14:07:30 +0900

    first commit

---
+++ src/test/java/org/example/newsfeedteamproject/NewsfeedTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.newsfeedteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedTeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
