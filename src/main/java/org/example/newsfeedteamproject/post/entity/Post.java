1ff38ca0149cc2a9d8985d1330374e3b49985ad4
jeongseung-won <jsw00415@naver.com>
2025-04-08 12:16:45 +0900

    feat : post 엔티티에 comment 연관관계 설정

--- src/main/java/org/example/newsfeedteamproject/post/entity/Post.java
+++ src/main/java/org/example/newsfeedteamproject/post/entity/Post.java
@@ -3,9 +3,14 @@ package org.example.newsfeedteamproject.post.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.newsfeedteamproject.comment.entity.Comment;
 import org.example.newsfeedteamproject.entity.BaseEntity;
 import org.example.newsfeedteamproject.post.dto.PostRequestDto;
 import org.example.newsfeedteamproject.user.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 포스트 정보를 나타내는 엔티티 클래스 입니다.
  * BaseEntity의 생성 시간 및 수정 시간을 상속 받습니다.
@@ -51,12 +56,17 @@ public class Post extends BaseEntity {
 //    private boolean like;
 
     /**
-     * 프로필 id 외래키
+     * 유저 연관관계
      */
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     private User user;
+
+    /**
+     * 코멘트 연관관계
+     */
+    @OneToMany(mappedBy = "post")
+    private final List<Comment> comments = new ArrayList<>();
 
     public Post(User user, PostRequestDto dto){
         this.user = user;
