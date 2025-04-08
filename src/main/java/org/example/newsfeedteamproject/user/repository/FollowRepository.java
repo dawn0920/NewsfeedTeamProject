935c0016ef100ca442fbce8854b023d95ac4b953
dawn0920 <saoul0920@gmail.com>
2025-04-08 21:34:24 +0900

    follow service 수정

--- src/main/java/org/example/newsfeedteamproject/user/repository/FollowRepository.java
+++ src/main/java/org/example/newsfeedteamproject/user/repository/FollowRepository.java
@@ -5,6 +5,8 @@ import org.example.newsfeedteamproject.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
@@ -16,4 +18,8 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
 
     // 언팔로우
     void deleteByFromUserAndToUser(User fromUser, User toUser);
+
+    // 특정 사용자의 팔로우 관계 조회
+    Optional<Follow> findByFromUserAndToUser(User fromUser, User toUser);
+
 }
