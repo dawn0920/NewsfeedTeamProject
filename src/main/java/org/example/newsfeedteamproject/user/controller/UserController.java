d81617ab8d26880b79e73d401eca3f0d71755a07
Master <baek011226@naver.com>
2025-04-08 16:04:34 +0900

    feat: passwordencoder 적용

--- src/main/java/org/example/newsfeedteamproject/user/controller/UserController.java
+++ src/main/java/org/example/newsfeedteamproject/user/controller/UserController.java
@@ -1,19 +1,12 @@
 package org.example.newsfeedteamproject.user.controller;
 
-import jakarta.servlet.http.HttpSession;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.example.newsfeedteamproject.user.dto.*;
-import org.example.newsfeedteamproject.user.dto.FollowRequestDto;
-import org.example.newsfeedteamproject.user.dto.FollowResponseDto;
-import org.example.newsfeedteamproject.user.entity.User;
 import org.example.newsfeedteamproject.user.service.FollowService;
-import org.example.newsfeedteamproject.user.entity.User;
 import org.example.newsfeedteamproject.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -37,37 +30,6 @@ public class UserController {
 
         return new ResponseEntity<>(userResponseDto, HttpStatus.CREATED);
     }
-
-    @PostMapping("/follow") // 팔로우
-    public  ResponseEntity<FollowResponseDto> followUser(
-            @RequestBody FollowRequestDto requestDto,
-            HttpSession session
-    ) {
-        User fromUser = (User)session.getAttribute("user");
-
-        if (fromUser == null) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "로그인 후 시도해주세요.");
-        }
-
-        FollowResponseDto responseDto = followService.follwoUser(fromUser, requestDto.getToUserId());
-        return ResponseEntity.ok(responseDto);
-    }
-
-    @DeleteMapping("/unfollow") // 언팔로우
-    public  ResponseEntity<FollowResponseDto> unfollowUser(
-            @RequestBody FollowRequestDto requestDto,
-            HttpSession session
-    ) {
-        User fromUser = (User)session.getAttribute("user");
-
-        if (fromUser == null) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "로그인 후 시도해주세요.");
-        }
-
-        FollowResponseDto responseDto = followService.unfollwoUser(fromUser, requestDto.getToUserId());
-        return ResponseEntity.ok(responseDto);
-    }
-
 
     /**
      * 회원 선택 조회 API
