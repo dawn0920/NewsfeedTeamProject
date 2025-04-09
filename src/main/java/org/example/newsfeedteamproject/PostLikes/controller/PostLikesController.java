9e5aca31a198eae207c5255df6fe1af59a81afae
dawn0920 <saoul0920@gmail.com>
2025-04-09 12:34:49 +0900

    like toggle(count 증가) 수정

--- src/main/java/org/example/newsfeedteamproject/PostLikes/controller/PostLikesController.java
+++ src/main/java/org/example/newsfeedteamproject/PostLikes/controller/PostLikesController.java
@@ -4,9 +4,8 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.example.newsfeedteamproject.PostLikes.dto.PostLikeResponseDto;
 import org.example.newsfeedteamproject.PostLikes.service.PostLikeService;
-import org.example.newsfeedteamproject.post.service.PostService;
 import org.example.newsfeedteamproject.user.entity.User;
-import org.example.newsfeedteamproject.user.service.UserService;
+import org.example.newsfeedteamproject.user.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,19 +19,22 @@ import org.springframework.web.server.ResponseStatusException;
 public class PostLikesController {
 
     private final PostLikeService postLikeService;
-    private final PostService postService;
-    private final UserService userService;
+    private final UserRepository userRepository;
 
     @PostMapping("/{postId}/like") // 좋아요 토글
     public PostLikeResponseDto toggleLike(
             @PathVariable Long postId,
             HttpSession session
     ) {
-        User fromUser = (User)session.getAttribute("user");
+        Long userId = (Long) session.getAttribute("LOGIN_USER");
 
-        if (fromUser == null) {
+        if (userId == null) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "로그인 후 시도해주세요.");
         }
+
+        // userId로 사용자 조회
+        User fromUser = userRepository.findById(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."));
 
         return postLikeService.toggleLike(fromUser, postId);
     }
