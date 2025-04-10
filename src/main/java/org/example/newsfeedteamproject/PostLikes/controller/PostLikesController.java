52ab64a236808f36bac4a82f3c38188b9f36a636
dawn0920 <saoul0920@gmail.com>
2025-04-10 12:48:53 +0900

    기존 Session에서  @SessionAttribute으로 변경

--- src/main/java/org/example/newsfeedteamproject/PostLikes/controller/PostLikesController.java
+++ src/main/java/org/example/newsfeedteamproject/PostLikes/controller/PostLikesController.java
@@ -4,13 +4,11 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.example.newsfeedteamproject.PostLikes.dto.PostLikeResponseDto;
 import org.example.newsfeedteamproject.PostLikes.service.PostLikeService;
+import org.example.newsfeedteamproject.global.consts.Const;
 import org.example.newsfeedteamproject.user.entity.User;
 import org.example.newsfeedteamproject.user.repository.UserRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -24,14 +22,8 @@ public class PostLikesController {
     @PostMapping("/{postId}/like") // 좋아요 토글
     public PostLikeResponseDto toggleLike(
             @PathVariable Long postId,
-            HttpSession session
+            @SessionAttribute(name = Const.LOGIN_USER) Long userId
     ) {
-        Long userId = (Long) session.getAttribute("LOGIN_USER");
-
-        if (userId == null) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "로그인 후 시도해주세요.");
-        }
-
         // userId로 사용자 조회
         User fromUser = userRepository.findById(userId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."));
